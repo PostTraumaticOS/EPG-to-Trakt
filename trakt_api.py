e16a362248669d5d77511d04a2931b2f0e2d1dea
NotAnExpert <51430625+PostTraumaticOS@users.noreply.github.com>
2024-02-25 12:30:18 -0600

    Update trakt_api.py
    Updated to include Movies, and shows that use "Episode 1; Episode 2; Episode 3" in description to fill up 30 minute time slots and only 7 minute episodes (e.g. Cartoons do this sometimes)

--- trakt_api.py
+++ trakt_api.py
@@ -1,6 +1,7 @@
 import json
 import requests
 from fuzzywuzzy import process
+from itertools import chain
 
 
 def read_config():
@@ -78,6 +79,25 @@ def get_access_token(authorization_code):
         return None
 
 
+def is_movie(program):
+    """
+    Check if the program is a movie.
+
+    Args:
+        program (dict): Program data.
+
+    Returns:
+        bool: True if the program is a movie, False otherwise.
+    """
+    for episode_key in ["episodeNum", "episodenum"]:
+        for episode in program.get(episode_key, []):
+            if episode.get("system") == "dd_progid":
+                value = episode.get("value", "")
+                if value[:2].lower() == "mv":
+                    return True
+    return False
+
+
 def get_first_show_id(show_title, access_token):
     """
     Get the ID of the first show matching the title.
@@ -113,7 +133,7 @@ def get_first_show_id(show_title, access_token):
         return None
 
 
-def get_trakt_episode_id_from_onscreen_value(show_title, onscreen_value, access_token):
+def search_episode_by_onscreen_value(show_title, onscreen_value, access_token):
     """
     Get Trakt episode ID using onscreen value.
 
@@ -123,8 +143,10 @@ def get_trakt_episode_id_from_onscreen_value(show_title, onscreen_value, access_
         access_token (str): Access token.
 
     Returns:
-        dict: Episode data.
+        dict: Episode data if found, None otherwise.
     """
+    season_number = ""
+    episode_number = ""
     try:
         season_number, episode_number = map(int, onscreen_value[1:].split('E'))
 
@@ -142,22 +164,24 @@ def get_trakt_episode_id_from_onscreen_value(show_title, onscreen_value, access_
         }
 
         response = requests.get(episode_summary_url, headers=headers)
+        if response.status_code == 404:
+            return None  # Episode not found
         response.raise_for_status()
         episode_data = response.json()
 
         return episode_data
 
     except requests.exceptions.RequestException as e:
-        print(f"Error occurred while fetching episode data for '{show_title}': {e}")
+        print(f"Error occurred while fetching S{season_number}E{episode_number} data in '{show_title}': {e}")
         return None
     except ValueError:
         print(f"Error: Invalid onscreen value '{onscreen_value}'.")
         return None
 
 
-def get_trakt_episode_id(show_title, episode_title, access_token):
+def search_episode_by_title(show_title, episode_title, access_token):
     """
-    Get Trakt episode ID using show and episode title.
+    Search for a specific episode by its title.
 
     Args:
         show_title (str): Title of the show.
@@ -165,7 +189,7 @@ def get_trakt_episode_id(show_title, episode_title, access_token):
         access_token (str): Access token.
 
     Returns:
-        dict: Episode data.
+        dict: Episode data if found, None otherwise.
     """
     search_api_url = f"https://api.trakt.tv/search/show?query={show_title}&type=show"
     headers = {
@@ -179,7 +203,7 @@ def get_trakt_episode_id(show_title, episode_title, access_token):
         response.raise_for_status()
         shows = response.json()
         for show in shows:
-            if show["show"]["title"] == show_title:
+            if process.extractOne(show_title.lower(), [show["show"]["title"].lower()])[1] > 90:
                 show_id = show["show"]["ids"]["trakt"]
                 seasons_api_url = f"https://api.trakt.tv/shows/{show_id}/seasons"
                 response = requests.get(seasons_api_url, headers=headers)
@@ -196,6 +220,44 @@ def get_trakt_episode_id(show_title, episode_title, access_token):
                             return episode
     except requests.exceptions.RequestException as e:
         print(f"Error occurred while fetching episode data for '{show_title}': {e}")
+        return None
+
+
+def search_movie_by_title_and_year(movie_title, movie_year, access_token):
+    """
+    Search for a movie by its title and release year.
+
+    Args:
+        movie_title (str): Title of the movie.
+        movie_year (str): Release year of the movie.
+        access_token (str): Access token.
+
+    Returns:
+        dict: Movie data if found, None otherwise.
+    """
+    search_api_url = f"https://api.trakt.tv/search/movie?query={movie_title}&type=movie"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + access_token,
+        "trakt-api-version": "2",
+        "trakt-api-key": CLIENT_ID
+    }
+    try:
+        response = requests.get(search_api_url, headers=headers)
+        response.raise_for_status()
+        movie_data = response.json()
+        if movie_data:
+            for movie in movie_data:
+                movie_info = movie['movie']
+                if movie_info['year'] == int(movie_year):
+                    return movie_info
+            print(f"Failed to find Trakt ID of the movie '{movie_title}' for the year '{movie_year}'")
+            return None
+        else:
+            print(f"Failed to find Trakt ID of the movie '{movie_title}'-'{movie_year}'")
+            return None
+    except requests.RequestException as e:
+        print(f"Error fetching movie data of '{movie_title}'-'{movie_year}':", {e})
         return None
 
 
@@ -225,12 +287,13 @@ def create_trakt_list(list_data, access_token):
         return None
 
 
-def add_to_trakt_list(list_id, episode_list, access_token):
+def add_to_trakt_list(list_id, movie_list, episode_list, access_token):
     """
     Add episodes to a Trakt list.
 
     Args:
         list_id (str): List ID.
+        movie_list (list): List of movie data to be added.
         episode_list (list): List of episodes.
         access_token (str): Access token.
 
@@ -245,14 +308,15 @@ def add_to_trakt_list(list_id, episode_list, access_token):
         "trakt-api-key": CLIENT_ID
     }
     data = {
+        "movies": movie_list,
         "episodes": episode_list
     }
     response = requests.post(add_items_url, headers=headers, json=data)
     if response.status_code == 201:
-        print("Episodes added to '{}' list".format(LIST_DATA['name']))
+        print("shows/movies added to '{}' Trakt list".format(LIST_DATA['name']))
         return True
     else:
-        print("Failed to add episode to Trakt list:", response.text)
+        print("Failed to add shows/movies to '{}' Trakt list:".format(LIST_DATA['name']))
         return False
 
 
@@ -268,16 +332,58 @@ def get_authorization_code():
     return input("Enter the authorization code from the URL: ")
 
 
-def main():
-    config_file = read_config()
-    if config_file is None:
-        return
+def get_trakt_episode_data(show_title, episode_title, onscreen_value, access_token):
+    """
+    Get Trakt episode data.
 
-    json_data = read_json_file(config['json_file_name'])
-    if json_data is None:
-        return
+    Args:
+        show_title (str): Title of the show.
+        episode_title (str): Title of the episode.
+        onscreen_value (str): Onscreen value.
+        access_token (str): Access token.
+
+    Returns:
+        list: List of episode data.
+    """
     episodes_list = []
+    if show_title and (episode_title or onscreen_value):
+        if onscreen_value:
+            episode = search_episode_by_onscreen_value(show_title, onscreen_value, access_token)
+            if not episode:
+                episode = search_episode_by_title(show_title, episode_title, access_token)
+                if episode:
+                    episodes_list.append({"ids": episode["ids"]})
+                else:
+                    print(f"'{onscreen_value}' in '{show_title}' not found")
+        else:
+            episode = search_episode_by_title(show_title, episode_title, access_token)
+            if (not episode) and (';' in episode_title):
+                episode_titles = episode_title.split(';')
+                for short_episode in episode_titles:
+                    episode = search_episode_by_title(show_title, short_episode.strip(), access_token)
+                    if episode:
+                        episodes_list.append({"ids": episode["ids"]})
+                    else:
+                        print(f"Failed to find Trakt ID for '{short_episode.strip()}' in '{show_title}'")
 
+        if episode:
+            episodes_list.append({"ids": episode["ids"]})
+        elif (not episode) and not (';' in episode_title):
+            print(f"Failed to find Trakt ID for '{episode_title}' in '{show_title}'")
+        return episodes_list
+    else:
+        print(f"Empty subtitle or onscreen_value for program: '{show_title}'")
+
+    return episodes_list
+
+
+def main():
+    """
+    Main function to orchestrate the Trakt list creation and data addition process.
+    """
+    json_data = read_json_file(config['json_file_name'])
+    movie_payload = []
+    episodes_payload = []
     auth_code = get_authorization_code()
     if auth_code:
         access_token = get_access_token(auth_code)
@@ -289,27 +395,32 @@ def main():
         if not list_id:
             print("Failed to create Trakt list")
             exit()
-
         for program in json_data.get("programs", []):
-            show_title = program.get("title", [""])[0].get("value")
-            episode_title = program.get("subTitle", [""])[0].get("value")
-            onscreen_value = next((item.get("value") for item in program.get("episodeNum", [])
-                                   if item.get("system") == "onscreen"), None)
-
-            if show_title and (episode_title or onscreen_value):
-                if onscreen_value:
-                    episode = get_trakt_episode_id_from_onscreen_value(show_title, onscreen_value, access_token)
+            if is_movie(program):
+                movie_title = program["title"][0]["value"] if program.get("title") else None
+                movie_year = program["date"] if program.get("date") else None
+                if movie_title and movie_year:
+                    movie_data = search_movie_by_title_and_year(movie_title.strip(), movie_year, access_token)
+                    if movie_data:
+                        movie_payload.append({"ids": movie_data["ids"]})
                 else:
-                    episode = get_trakt_episode_id(show_title, episode_title, access_token)
-
-                if episode:
-                    episodes_list.append({"ids": episode["ids"]})
-                else:
-                    print(f"Failed to find Trakt ID for '{episode_title}'")
+                    print(f"Both movie title and year are required to search for movie data of '{movie_title}'.")
             else:
-                print(f"Empty subtitle or onscreen_value for program: '{show_title}'")
+                show_title = program["title"][0]["value"] if program.get("title") else None
+                episode_title = ""
+                # Check if "subTitle" or "subtitle" exists
+                if "subTitle" in program:
+                    episode_title = program.get("subTitle", [""])[0].get("value") if program.get("subTitle") else None
+                elif "subtitle" in program:
+                    episode_title = program.get("subtitle", [""])[0].get("value") if program.get("subtitle") else None
+                onscreen_value = next((item.get("value") for item in program.get("episodeNum", [])
+                                       if item.get("system") == "onscreen"), None)
+                episodes_list = get_trakt_episode_data(show_title, episode_title, onscreen_value, access_token)
+                episodes_payload.append(episodes_list)
 
-        add_to_trakt_list(list_id, episodes_list, access_token)
+        # Flatten the episodes_payload list
+        episodes_payload = list(chain.from_iterable(episodes_payload))
+        add_to_trakt_list(list_id, movie_payload, episodes_payload, access_token)
     else:
         print("Failed to obtain authorization code")
         exit()
